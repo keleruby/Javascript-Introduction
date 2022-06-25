d9116a05e11f1fe6edeb4b746a3d08e2994d58c1
Kelechi Blessing <105248040+keleruby@users.noreply.github.com>
2022-06-25 01:47:58 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+ let myName = "Name:  Kelechi Blessing ";
+      let myHeight = "Height: 5ft 6inches ";
+      let myCountry = "Country: Nigeria";
+window.alert(myName + "  " + myHeight + "  " + myCountry);
